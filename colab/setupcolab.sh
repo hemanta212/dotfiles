f221d6cbfcea6c81b0abcc4533d19470c3a45cac
hemanta212 <sharmahemanta.212@gmail.com>
2020-09-20 12:32:45 +0000

    updates script

--- colab/setupcolab.sh
+++ colab/setupcolab.sh
@@ -3,13 +3,13 @@ git clone https://github.com/tmux-plugins/tpm ~/.tmux/plugins/tpm --depth 1
 git clone https://github.com/hemanta212/dotfiles ~/dev/dotfiles
 
 # setup exit script
-curl -sSL https://hemanta212.github.io/dotfiles/colab/exit.sh -o ~/
-chmod u+x exit.sh
+curl -sSL https://hemanta212.github.io/dotfiles/colab/exit.sh -o ~/exit.sh
+chmod u+x ~/exit.sh
 
 
 # tmux and nvim init files
 cp ~/dev/dotfiles/tmux/.tmux.conf ~/
-curl -sSL https://hemanta212.github.io/dotfiles/colab/resurrect -o ~/.tmux/resurrector.zip 
+curl -sSL https://hemanta212.github.io/dotfiles/colab/resurrect.zip -o ~/.tmux/resurrector.zip 
 cd ~/.tmux/ && unzip resurrector.zip
 cd 
 
@@ -21,10 +21,10 @@ cp -r ~/dev/dotfiles/zsh/zsh ~/.config/
 git clone https://github.com/romkatv/powerlevel10k ~/.config/zsh/powerlevel10k --depth 1
 
 # Prepare setup scripts
-curl -sSL https://hemanta212.github.io/dotfiles/colab/.p10k.zsh -o ~/.config/zsh/p10k.zsh
+curl -sSL https://hemanta212.github.io/dotfiles/colab/p10k.zsh -o ~/.config/zsh/p10k.zsh
 cd ~/dev/dotfiles/zsh
 cp .pythonrc.py ~/.config/ptpython/
-cp .zshrc .bashrc ~/
+cp .zshrc .bashrc .bash_profile ~/
 cd 
 
 # setup git
