f1aae40c584e8577b8e1c5702d5d02684ad72df6
hemanta212 <sharmahemanta.212@gmail.com>
2020-09-30 11:47:32 +0000

    adds config

--- colab/c.sh
+++ colab/c.sh
@@ -2,5 +2,6 @@ cd ~/dev/dotfiles
 git config credential.helper store
 git push origin master
 cd ~/dev/
-git clone https://github.com/hemanta212/c-practice 
+git clone https://github.com/hemanta212/c-practice
+git clone https://github.com/hemanta212/personal
 cd c-practice/clisp
