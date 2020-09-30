02583ff80a5d0690ce97d9e3e2f941e0396639c8
hemanta212 <sharmahemanta.212@gmail.com>
2020-09-30 13:09:04 +0000

    updates config

--- emacs/.doom.d/config.el
+++ emacs/.doom.d/config.el
@@ -6,8 +6,8 @@
 
 ;; Some functionality uses this to identify you, e.g. GPG configuration, email
 ;; clients, file templates and snippets.
-(setq user-full-name "John Doe"
-      user-mail-address "john@doe.com")
+(setq user-full-name "Hemanata Sharma"
+      user-mail-address "sharmahemanta.212@gmail.com")
 
 ;; Doom exposes five (optional) variables for controlling fonts in Doom. Here
 ;; are the three important ones:
@@ -34,6 +34,9 @@
 ;; This determines the style of line numbers in effect. If set to `nil', line
 ;; numbers are disabled. For relative line numbers, set this to `relative'.
 (setq display-line-numbers-type t)
+
+;; VIM escape seq kj
+(setq evil-escape-key-sequence "kj")
 
 
 ;; Here are some additional functions/macros that could help you configure Doom:
