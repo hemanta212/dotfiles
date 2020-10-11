1b6bec228c260647933bdfffc88b3df53047717e
hemanta212 <sharmahemanta.212@gmail.com>
2020-10-11 08:36:16 +0000

    save ressurect file

--- colab/exit.sh
+++ colab/exit.sh
@@ -3,7 +3,7 @@ cd ~/.tmux/
 zip resurrector resurrect/ -r 
 cd ~/dev/dotfiles
 git checkout gh-pages
-mv resurrector.zip ~/dev/dotfiles/colab/resurrect.zip
+mv ~/.tmux/resurrector.zip ~/dev/dotfiles/colab/resurrect.zip
 git commit -a -m "save ressurect file"
 git config credential.helper store
 git push origin gh-pages
