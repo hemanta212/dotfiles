8c9727823f3f260ecb8cfb375c27ed34881bd90c
hemanta212 <sharmahemanta.212@gmail.com>
2020-11-03 14:31:21 +0000

    updates colab scripts

--- colab/flutter.sh
+++ colab/flutter.sh
@@ -1,4 +1,35 @@
 mkdir ~/dev
+cd ~/dev
+git clone https://github.com/hemanta212/hello_flutter.git
+git clone https://github.com/hemanta212/inventory_app.git
+cd hello_flutter/
+git config --global credential.helper store
+git push origin master
+cd ~/dev
+
+git clone https://github.com/hemanta212/samachar_app.git
+git clone https://github.com/hemanta212/meme_khani.git
+git clone https://github.com/hemanta212/marriage-card-tracker.git card_tracker
+
+cd card_tracker 
+git checkout next
+cp ~/dev/personal/credentials/marriage_card_tracker/* android/app/
+mv android/app/key.properties android/
+cd 
+cp dev/card_tracker ~/  -r
+
+
+# Flutter
+git clone https://github.com/flutter/flutter ~/local/flutter
+cd ~/local/flutter && git checkout beta && cd
+export PATH="$PATH":"$HOME/local/flutter/bin/"
+flutter channel beta
+flutter upgrade
+flutter doctor
+yes | flutter doctor --android-licenses
+flutter config --enable-web
+flutter doctor
+
 # Get sdkmanager cli and install android-sdk
 wget https://dl.google.com/android/repository/commandlinetools-linux-6609375_latest.zip
 unzip commandlinetools-linux-6609375_latest.zip
@@ -13,31 +44,6 @@ export ANDROID_HOME=$HOME/local/android-sdk
 export PATH=$PATH:$ANDROID_HOME/cmdline-tools/tools/bin
 export PATH=$PATH:$ANDROID_HOME/platform-tools
 
-yes | sdkmanager "platform-tools"  "build-tools;28.0.3" "platforms;android-28"
+yes | sdkmanager "platform-tools"  "build-tools;28.0.3" "platforms;android-29"
 
-# Flutter
-git clone https://github.com/flutter/flutter ~/local/flutter
-cd ~/local/flutter && git checkout stable && cd
-export PATH="$PATH":"$HOME/local/flutter/bin/"
-flutter
-flutter doctor
-yes | flutter doctor --android-licenses
-flutter doctor
 
-cd ~/dev
-git clone https://github.com/hemanta212/hello_flutter.git
-git clone https://github.com/hemanta212/inventory_app.git
-cd hello_flutter/
-git config --global credential.helper store
-git push origin master
-cd ~/dev
-
-git clone https://github.com/hemanta212/samachar_app.git
-git clone https://github.com/hemanta212/meme_khani.git
-git clone https://github.com/hemanta212/marriage-card-tracker.git card_tracker
-cd card_tracker 
-git checkout next
-cp ~/dev/personal/credentials/marriage_card_tracker/* android/app/
-mv android/app/key.properties android/
-cd 
-cp dev/card_tracker ~/  -r
--- colab/python.sh
+++ colab/python.sh
@@ -5,19 +5,19 @@ git config --global credential.helper store
 git push origin master
 cd
 
-curl -sSL https://raw.githubusercontent.com/python-poetry/poetry/master/get-poetry.py -o ~/poetry.py
-curl -sSL https://hemanta212.github.io/blogger-cli/get_blogger.py -o ~/blogger.py
-python ~/poetry.py -y
-poetry config virtualenvs.in-project true
-
-python ~/blogger.py -y 
-blogger addblog a -s
-
-poetry init -n -q
-poetry add requests ptpython pylint black python-language-server
-
 cd ~/dev
 git clone https://github.com/hemanta212/blogger-cli
 git clone https://github.com/hemanta212/news_api
 git clone https://github.com/hemanta212/meme_khani_api
 git clone https://github.com/hemanta212/status
+
+curl -sSL https://raw.githubusercontent.com/python-poetry/poetry/master/get-poetry.py -o ~/poetry.py
+curl -sSL https://hemanta212.github.io/blogger-cli/get_blogger.py -o ~/blogger.py
+python ~/poetry.py -y
+poetry config virtualenvs.in-project true
+
+poetry init -n -q
+poetry add requests ptpython pylint black python-language-server
+
+python ~/blogger.py -y 
+blogger addblog a -s
