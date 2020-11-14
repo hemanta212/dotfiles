b23aba31cbe47edc3979fc9436e4c8ff85696634
hemanta212 <sharmahemanta.212@gmail.com>
2020-11-14 22:49:30 +0545

    Updates colab setup script

--- colab/setupcolab.sh
+++ colab/setupcolab.sh
@@ -3,12 +3,13 @@ git clone https://github.com/tmux-plugins/tpm ~/.tmux/plugins/tpm --depth 1
 git clone https://github.com/hemanta212/dotfiles ~/dev/dotfiles
 
 # setup exit script
-curl -sSL https://hemanta212.github.io/dotfiles/colab/exit.sh -o ~/exit.sh
-curl -sSL https://hemanta212.github.io/dotfiles/colab/manim.sh -o ~/manim.sh
+wget https://hemanta212.github.io/dotfiles/colab/exit.sh
+wget https://hemanta212.github.io/dotfiles/colab/manim.sh
 chmod u+x ~/exit.sh
+chmod u+x ~/manim.sh
 
 # tmux and nvim init files
-cp ~/dev/dotfiles/tmux/.tmux.conf ~/
+ln -s ~/dev/dotfiles/tmux/.tmux.conf ~/.tmux.conf
 curl -sSL https://hemanta212.github.io/dotfiles/colab/resurrect.zip -o ~/.tmux/resurrector.zip 
 cd ~/.tmux/ && unzip resurrector.zip
 cd 
@@ -22,15 +23,20 @@ cp ~/dev/dotfiles/i3 -r ~/.i3
 echo "i3" > .xsession
 
 # zsh dotfiles
-cp -r ~/dev/dotfiles/zsh/zsh ~/.config/
-rm ~/.config/zsh/.mytermuxrc
-git clone https://github.com/romkatv/powerlevel10k ~/.config/zsh/powerlevel10k --depth 1
+ln -s ~/dev/dotfiles/zsh/zsh ~/.config/zsh
+cd ~/.config/zsh
+wget https://hemanta212.github.io/dotfiles/colab/p10k.zsh
+mkdir zsh-plugins
+git clone --depth=1 https://github.com/romkatv/powerlevel10k.git
+git clone --depth=1 https://github.com/zsh-users/zsh-syntax-highlighting.git
+git clone --depth=1 https://github.com/zsh-users/zsh-autosuggestions.git
+git clone --depth=1 https://github.com/zsh-users/zsh-history-substring-search.git
+git clone --depth=1 https://github.com/agkozak/zsh-z.git
 
 # Prepare setup scripts
-curl -sSL https://hemanta212.github.io/dotfiles/colab/p10k.zsh -o ~/.config/zsh/p10k.zsh
-cd ~/dev/dotfiles/zsh
-cp .pythonrc.py ~/.config/ptpython/
-cp .zshrc .bashrc .bash_profile ~/
+ln -s ~/dev/dotfiles/zsh/.pythonrc.py ~/.config/ptpython/.pythonrc.py
+ln -s ~/dev/dotfiles/zsh/.zshrc ~/.zshrc
+ln -s ~/dev/dotfiles/zsh/.bash_profile ~/.bash_profile
 cd 
 
 # setup git
