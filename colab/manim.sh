05d7bad7df25113eddfdb788f320c8e473258a83
hemanta212 <sharmahemanta.212@gmail.com>
2020-12-22 16:55:41 +0000

    Update manim setup script

--- colab/manim.sh
+++ colab/manim.sh
@@ -1,6 +1,6 @@
 cd dev/hello-manim
 
-sudo apt-add-repository ppa:jonathonf/texlive-2019 && sudo apt-get update  && sudo apt-get upgrade
+#sudo apt-add-repository ppa:jonathonf/texlive-2019 && sudo apt-get update  && sudo apt-get upgrade
 sudo apt-get install python3-opencv libcairo2-dev texlive texlive-latex-extra texlive-fonts-extra \
 texlive-latex-recommended texlive-science texlive-fonts-extra tipa
 
