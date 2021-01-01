90b0c0f39b0b5984d8f4864e5fa6628ef37b5aac
hemanta212 <sharmahemanta.212@gmail.com>
2021-01-01 07:09:48 +0000

    Updates emacs build script

--- colab/c.sh
+++ colab/c.sh
@@ -4,5 +4,8 @@ git clone https://github.com/hemanta212/personal
 git clone https://github.com/hemanta212/hello-manim
 cd 
 
-git clone --depth 1 https://github.com/hlissner/doom-emacs ~/.emacs.d 
-yes | ~/.emacs.d/bin/doom install
+#git clone --depth 1 https://github.com/hlissner/doom-emacs ~/.emacs.d 
+#yes | ~/.emacs.d/bin/doom install
+rm -rf .emacs.d
+cp -r ~/dev/dotfiles/emacs/.emacs.d ~/
+emacs
