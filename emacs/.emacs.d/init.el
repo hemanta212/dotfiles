c6bb66824fcdb836a5faab43155b8b4d9bbf8be5
hemanta212 <sharmahemanta.212@gmail.com>
2021-06-10 17:22:35 +0545

    Update emacs config

--- emacs/.emacs.d/init.el
+++ emacs/.emacs.d/init.el
@@ -18,24 +18,25 @@
 
 (setq inhibit-startup-message t)
 
-(scroll-bar-mode -1)        ; Disable visible scrollbar
-(tool-bar-mode -1)          ; Disable the toolbar
-(tooltip-mode -1)           ; Disable tooltips
-(set-fringe-mode 10)        ; Give some breathing room
+(if (display-graphic-p)
+    (progn
+      (set-fringe-mode 10)        ; Give some breathing room
+      (tooltip-mode -1)           ; Disable tooltips
+      (tool-bar-mode -1)
+      (set-face-attribute 'default nil :font "Fira Code Retina" :height 130)
+      (scroll-bar-mode -1)))
 
 (menu-bar-mode -1)            ; Disable the menu bar
-
 ;; Set up the visible bell
 (setq visible-bell t)
 
-(set-face-attribute 'default nil :font "Fira Code Retina" :height 130)
 (dolist (mode '(org-mode-hook
                 term-mode-hook
-		  shell-mode-hook
+                  shell-mode-hook
                 treemacs-mode-hook
-		  eshell-mode-hook))
+                  eshell-mode-hook))
   (add-hook mode (lambda () (display-line-numbers-mode 0))))
-  
+
 (column-number-mode)
 (global-display-line-numbers-mode t)
 
@@ -315,3 +316,16 @@
 
 (use-package evil-nerd-commenter
   :bind ("M-/" . evilnc-comment-or-uncomment-lines))
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(package-selected-packages
+   '(all-the-icons-ivy yasnippet which-key visual-fill-column use-package typescript-mode sicp rainbow-delimiters python-mode poetry org-bullets magit-delta lsp-ui lsp-treemacs lsp-pyright lsp-ivy ivy-rich htmltagwrap helpful general forge evil-nerd-commenter evil-magit evil-escape evil-collection doom-themes doom-modeline counsel-projectile company-web company-box command-log-mode)))
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ )
