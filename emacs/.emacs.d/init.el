b171e18de4dafbc46be6abee2ef22e0adc35c698
hemanta212 <sharmahemanta.212@gmail.com>
2021-06-10 15:54:20 +0545

    Fix init.el ( bug

--- emacs/.emacs.d/init.el
+++ emacs/.emacs.d/init.el
@@ -19,7 +19,7 @@
 (setq inhibit-startup-message t)
 
 (scroll-bar-mode -1)        ; Disable visible scrollbar
-tool-bar-mode -1)          ; Disable the toolbar
+(tool-bar-mode -1)          ; Disable the toolbar
 (tooltip-mode -1)           ; Disable tooltips
 (set-fringe-mode 10)        ; Give some breathing room
 
