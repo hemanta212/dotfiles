cd59f54460d56d18efa42d77f43a9cc64f73ef46
hemanta212 <sharmahemanta.212@gmail.com>
2021-06-11 08:05:34 +0545

    Adds termux configs

--- setup.sh
+++ setup.sh
@@ -1,22 +1,17 @@
+mkdir -p ~/.config/ && mkdir -p ~/.config/nvim && mkdir -p ~/.config/ptpython && mkdir -p ~/.config/zsh/
+
 # Clone resources 
 git clone https://github.com/tmux-plugins/tpm ~/.tmux/plugins/tpm --depth 1
 git clone https://github.com/hemanta212/dotfiles ~/dev/dotfiles
-
-cp ~/dev/dotfiles/tmux/.tmux.conf ~/
-
-mkdir ~/.config/ && mkdir ~/.config/nvim && mkdir ~/.config/ptpython
-curl -sLf https://spacevim.org/install.sh | bash
-cp ~/dev/dotfiles/vim/space-vim/.SpaceVim.d/ -r ~/
-
-# zsh dotfiles
-cp -r ~/dev/dotfiles/zsh/zsh ~/.config/
 git clone https://github.com/romkatv/powerlevel10k ~/.config/zsh/powerlevel10k --depth 1
 
 curl -sSL https://hemanta212.github.io/dotfiles/colab/p10k.zsh -o ~/.config/zsh/p10k.zsh
-cd ~/dev/dotfiles/zsh
-cp .pythonrc.py ~/.config/ptpython/
-cp .zshrc .bashrc .bash_profile ~/
-cd 
+
+ln -s ~/dev/dotfiles/tmux/.tmux.conf ~/
+DOT_PREFIX = "~/dev/dotfiles/zsh"
+ln -s ~/dev/dotfiles/zsh/zsh/* ~/.config/zsh/
+ln -s $DOT_PREFIX/pythonrc.py ~/.config/ptpython/
+ln -s  $DOT_PREFIX/.zshrc $DOT_PREFIX/.bashrc $DOT_PREFIX/.bash_profile ~/
 
 # setup git
 cd ~/dev/dotfiles
