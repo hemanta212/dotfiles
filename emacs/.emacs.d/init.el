053664d74ee8b879325b0d4a2cfe41466d48496b
hemanta212 <sharmahemanta.212@gmail.com>
2022-08-30 22:34:57 +0545

    Restore compilation font size

--- emacs/.emacs.d/init.el
+++ emacs/.emacs.d/init.el
@@ -174,14 +174,14 @@
 (defun my-buffer-face-mode-variable ()
   "Set font to a variable width (proportional) fonts in current buffer"
   (interactive)
-  (setq buffer-face-mode-face '(:family "Consolas" :height 70 :width semi-condensed))
+  (setq buffer-face-mode-face '(:family "Consolas" :height 120 :width semi-condensed))
   (buffer-face-mode))
 
 ;; Use monospaced font faces in current buffer
 (defun my-buffer-face-mode-fixed ()
   "Sets a fixed width (monospace) font in current buffer"
   (interactive)
-  (setq buffer-face-mode-face '(:height 80))
+  (setq buffer-face-mode-face '(:height 120))
   (buffer-face-mode))
 
 (add-hook 'compilation-mode-hook 'my-buffer-face-mode-variable)
